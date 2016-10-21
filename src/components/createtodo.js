d4b264ce683cb70364e02f65b428a6619a192e83
Marcode777 <marcosebello7@gmail.com>
2016-10-21 06:40:13 -0400

    further implemented setStates, so a state is set, implemented onClick functions with buttons so that when a button is clicked, the state is changed to editing, setc.,these buttons implemented are edit, save, cancel, etc., modified src/components/createtodo.js, src/components/todoslistitem.js, README.md

--- src/components/createtodo.js
+++ src/components/createtodo.js
@@ -6,10 +6,19 @@ export default class TodosList extends React.Component{
 
   render() {
     return(
-      <form>
-        <input type="text" placeholder="Tasks to do..."/>
+      <form onSubmit={this.handleCreate.bind(this)}>
+        <input type="text" placeholder="Tasks to do..." ref="createInput"/>
         <button>Create</button>
       </form>
       );
   }
+
+  handleCreate(event){
+    event.preventDefault();
+    console.log(this.refs.createInput.value);
+  }
 }
+
+// a way to grab the value from the input is via refs, which is basically an identifier for the DOM element
+// keep in mind that onSubmit's default behavior is to refresh the page, so to prevent that, we do an event.preventDefault(); so we first pass an event and then the event.preventDefault();
+// continuing on to the refs, if we take the value, then it will actually return the actual value from the user input, so this is how we're going to grab the value from the input box
